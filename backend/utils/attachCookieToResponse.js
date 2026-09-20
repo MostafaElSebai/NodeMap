ebdc453b32806930f149cce0fffd020f6b87b0c6
Mostafa El Sebai <smskees20@gmail.com>
2026-09-20 21:37:31 +0300

    first commit

--- backend/utils/attachCookieToResponse.js
+++ backend/utils/attachCookieToResponse.js
@@ -9,6 +9,7 @@ export const attachCookieToResponse = ({ res, user }) => {
     res.cookie('token', token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
         maxAge: thirtyDays,
         signed: true
     })
